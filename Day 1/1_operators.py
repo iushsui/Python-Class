3949865ca3d173796e391cac6de1b172325c121a
iushsui <ayush.prajapati07@gmail.com>
2024-01-07 18:12:10 +0545

    operators and syntax

---
+++ Day 1/1_operators.py
@@ -0,0 +1,45 @@
+# operatoes are the special symbols to perform operations in python or in any programming language
+# python alos has its set of operators
+
+# Arthematic
+# Logical 
+# Relational / Comparison
+# membership
+# identiy
+# assignment
+
+# Arthematic (+, -, /, *, %, //, **)
+# % => remainder of dovosopm operatin
+# (floor division) //
+# exponent operator
+
+# Logical operators (and, or, not)
+
+# Comparison operators (>, <, >=, <=, ==, !=)
+# result is either true or false 
+
+# memebership operator ("in", "not in")
+print(2 in [1, 2, 3, 4]) # True
+print(2 not in [1, 2, 3, 4]) # False
+
+# identity operator ("is"., "is not")
+a=1
+b=2
+print (a == b) #True
+print (a is b) #True valie of a nd b are in the same memory location
+
+a=[]
+b=[]
+print (a == b) #True
+print (a is b) #False valie of a nd b are in the same memory location
+
+# (=, +=, -=, /=)
+# a = a + 1 is same as a += 1
+
+# precendence is is the rule which defiens whoch opration to execute firsl
+
+# if two operators have the same precenndence then associativity is taken into consideration
+# associativuty in pthing goes from left to right
+
+# but only exception is ** oprator, is goes from right to left
+a = 3 ** 2 ** 2
---
+++ Day 1/2_syntax.py
@@ -0,0 +1,12 @@
+# identifiers => all those names which are given by the user while coding are the identifiers
+# example => variavles, function name, class name, module name (name of file, here 2_sytax.py)
+
+#rules for naming identifiers
+# it is case sensitive ( a and A is diffenrent)
+# identifiers cant be keywords (there are around 35 keywords in python, help(keywords) -  to view keywords)
+# identifiers cant contain special symbols (@, #, $, %, ^)
+# identifiers can contain numbers but they cant start with number
+# _ is valid everywhere
+
+# python statement
+# generally single line is python statement but sometimes single statement can be in multiple lines, and muliple statement can occur in same line
